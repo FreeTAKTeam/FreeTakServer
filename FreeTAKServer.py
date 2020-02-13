ca4d7c744aa1a8540e16f81618b3727afb37fa59
naman108 <natha.paquette@gmail.com>
2020-02-13 19:47:03 -0400

    Add files via upload

---
+++ FreeTAKServer.py
@@ -0,0 +1,28 @@
+import socketserver
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+    def handle(self):
+        while 1:
+            self.data = self.request.recv(1024)
+            if not self.data:
+                break
+            self.data = self.data.strip()
+            print(str(self.client_address[0]) + " wrote: ")
+            print (self.data)
+            self.request.send(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "192.168.0.19", 8087
+
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
