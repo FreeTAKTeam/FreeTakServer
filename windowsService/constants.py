c629200dfb43154275035168dec73bcb22a36001
naman108 <natha.paquette@gmail.com>
2020-03-23 20:48:22 -0300

    Create constants.py
    constants file

---
+++ windowsService/constants.py
@@ -0,0 +1,7 @@
+class vars():
+   def __init__(self):
+       self.PING = 'ping'
+       self.GEOCHAT = 'GeoChat'
+       self.FAIL = 'fail'
+       self.EMPTY_BYTE = b''
+       self.DEFAULTPORT = 8087
