79ce45730da02f14077b199901b6ce4fb0b38116
naman108 <natha.paquette@gmail.com>
2020-03-23 20:05:12 -0300

    Update and rename beginProgramAsService to beginProgramAsService.py

---
+++ windowsService/beginProgramAsService.py
@@ -0,0 +1,42 @@
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+import time
+from TAKFreeServer_running_as_service import ThreadedServer
+import os.path
+import threading
+import sys
+class AppServerSvc (win32serviceutil.ServiceFramework):
+    _svc_name_ = "TestService"
+    _svc_display_name_ = "Test Service"
+    killSwitch = 0
+
+    def __init__(self,args):
+        win32serviceutil.ServiceFramework.__init__(self,args)
+        self.hWaitStop = win32event.CreateEvent(None,0,0,None)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+        self.killSwitch = 1
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,servicemanager.PYS_SERVICE_STARTED,(self._svc_name_,''))
+        self.main()
+
+    def main(self):
+        t = threading.Thread(target = ThreadedServer('',8087).listen, args = (), daemon=True)
+        x = open('C:/Users/Stephen Paquette/Documents/TAKFreeServerVersions/logs.txt', 'w')
+        x.write('thread starting')
+        x.close
+        t.start()
+        while True:
+            if self.killSwitch == 1:
+                sys.exit()
+            else:
+                True
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(AppServerSvc)
