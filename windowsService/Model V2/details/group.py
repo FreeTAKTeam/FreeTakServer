f75c77dc1e8b2a1b43bc3edc2847569423c9834d
naman108 <natha.paquette@gmail.com>
2020-03-30 22:35:41 -0300

    Add files via upload

---
+++ windowsService/Model V2/details/group.py
@@ -0,0 +1,16 @@
+class __group:
+    def __init__(self):
+        self.name = ''
+        self.group = ''
+
+    def getgroup(self):
+        return self.group
+
+    def setgroup(self, group):
+        self.group = group
+
+    def getname(self):
+        return self.name
+
+    def setname(self, name):
+        self.name = name
