fd19298b567e9e35cf370824c5d5199b0c532429
naman108 <natha.paquette@gmail.com>
2020-04-10 18:07:05 -0300

    Create constant.py

---
+++ TAKfreeServer/TAKLinuxService/constant.py
@@ -0,0 +1,10 @@
+class vars():
+   def __init__(self):
+       self.PING = 'ping'
+       self.GEOCHAT = 'GeoChat'
+       self.FAIL = 'fail'
+       self.EMPTY_BYTE = b''
+       self.DEFAULTPORT = 8087
+       self.LOGFILEPATH = 'path'
+       self.HOSTIP = 'ip'
+       self.BUFFER = 16384
