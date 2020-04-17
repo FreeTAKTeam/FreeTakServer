2971f55e60bc12fa6aceda515c1494111a459278
naman108 <natha.paquette@gmail.com>
2020-04-17 11:56:25 -0300

    Add files via upload

---
+++ TAKfreeServer/module1.py
@@ -0,0 +1,35 @@
+import sys
+import time
+import os
+
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1. / 500)
+os.system('color a')
+slowprint(r'''\
+*** ***       FREE TAK SERVER        *** ***       
+            .------.
+           /  ~ ~   \,------.      ______
+         ,'  ~ ~ ~  /  (@)   \   ,'      \
+       ,'          /`.    ~ ~ \ /         \
+     ,'           | ,'\  ~ ~ ~ X     \  \  \
+   ,'  ,'          V--<       (       \  \  \
+ ,'  ,'               (vv      \/\  \  \  |  |
+(__,'  ,'   /         (vv   ""    \  \  | |  |
+  (__,'    /   /       vv   """    \ |  / / /
+      \__,'   /  |     vv          / / / / /
+          \__/   / |  | \         / /,',','
+             \__/\_^  |  \       /,'',','\
+                    `-^.__>.____/  ' ,'   \
+                            // //---'      |
+          ===============(((((((=================
+                                     | \ \  \
+                                     / |  |  \
+                                    / /  / \  \
+                                    `.     |   \
+                                      `--------'
+
+     - The parrot's not dead! It's just resting!- 
+''')
