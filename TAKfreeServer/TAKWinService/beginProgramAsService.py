48b7758a4df40065baef5053ca2fdb949b2452cf
Corvo <60719165+brothercorvo@users.noreply.github.com>
2020-05-13 18:18:44 -0300

    Update beginProgramAsService.py

--- TAKfreeServer/TAKWinService/beginProgramAsService.py
+++ TAKfreeServer/TAKWinService/beginProgramAsService.py
@@ -12,7 +12,7 @@ PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 import server
-import httpser
+import httpserver
 class PythonCornerExample(SMWinservice):
     _svc_name_ = "FTS main service"
     _svc_display_name_ = "FTS main service"
