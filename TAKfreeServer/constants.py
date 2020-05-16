4c2403d6f1f56888e45916a024a8b6eb0d47a784
naman108 <natha.paquette@gmail.com>
2020-05-16 15:50:53 -0300

    Update constants.py
    added comments

--- TAKfreeServer/constants.py
+++ TAKfreeServer/constants.py
@@ -8,20 +8,32 @@ class vars():
        self.GEOCHAT = 'GeoChat'
        self.FAIL = 'fail'
        self.EMPTY_BYTE = b''
+       #Default tcp port change to change the port your FTS is listening on
        self.DEFAULTPORT = 8087
        self.LOGFILEPATH = 'log.log'
+       #The buffer to receive the clients initial connection data
        self.STARTBUFFER = 32784
-       self.BUFFER =513
+       #The default buffer to dynamically recieve data from client
+       self.BUFFER =8192
+       #The delay in seconds between the server sending data to each indevidual client
        self.DELAY = 5
+       #your current ip
        self.IP = 'Your IP'
+       #Delay for the bandaid in server.py
        self.RENEWTIME = 1
        #logging config
        self.LOGTIMEFORMAT = '%(levelname)s:%(asctime)s:%(message)s:%(lineno)d'
+       #the name of the log
        self.LOGNAME = 'FTS'
+       #name of warning log file
        self.WARNINGLOG = 'FTS_warning.log'
+       #name of debug log file
        self.DEBUGLOG = 'FTS_debug.log'
+       #name of info log file
        self.INFOLOG = 'FTS_info.log'
+       #delimiter between connection data DO NOT TOUCH
        self.DELIMITER = ' ? '
+       #maximum size of each log file
        self.MAXFILESIZE = 100000
        self.BACKUPCOUNT = 5
        #http server config
