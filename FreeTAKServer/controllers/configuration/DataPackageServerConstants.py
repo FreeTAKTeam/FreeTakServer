9ae0215ac0e07fcee77cf11d7ad90e604a792728
naman108 <natha.paquette@gmail.com>
2020-06-20 16:16:42 -0300

    Update DataPackageServerConstants.py
    updated version

--- FreeTAKServer/controllers/configuration/DataPackageServerConstants.py
+++ FreeTAKServer/controllers/configuration/DataPackageServerConstants.py
@@ -18,6 +18,6 @@ class DataPackageServerConstants:
        self.HTTPDEBUG = False
        self.HTTPMETHODS = ['POST', 'GET', 'PUT']
 
-       self.versionInfo = 'FreeTAKServer-0.8.4.5-Beta'
+       self.versionInfo = 'FreeTAKServer-0.8.4.6-Beta'
        self.NodeID = 'Public-FTS'
        self.VERSIONJSON = '{"version":"2","type":"ServerConfig", "data":{"version": "%s", "api": "2","hostname":"%s"}, "nodeId":"%s"}' % (self.versionInfo, "0.0.0.0" ,self.NodeID)
