02499d8e0d751f71818ff4abd3b473d0374715fa
naman108 <natha.paquette@gmail.com>
2020-07-03 21:22:45 -0300

    added model files

---
+++ FreeTAKServer/controllers/model/SendEmergency.py
@@ -0,0 +1,8 @@
+class SendEmergency:
+    def __init__(self):
+        self.clientInformation = ''
+        self.xmlString = ''
+        self.placeInternalArray = True
+        self.status = ''
+        self.type = "emergency"
+        self.modelObject = ''
---
+++ FreeTAKServer/controllers/model/Uid.py
@@ -0,0 +1,7 @@
+class Uid:
+    def __init__(self):
+        self.Droid = ''
+    def getDroid(self):
+        return self.Droid
+    def setDroid(self, Droid):
+        self.Droid = Droid
