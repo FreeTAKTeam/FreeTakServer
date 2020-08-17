0085cfe49b06892dfdb2e84ed8bae8c4ef453620
naman108 <natha.paquette@gmail.com>
2020-08-17 19:24:49 -0300

    Delete ClientReceptionLoggingConstants.py

--- FreeTAKServer/controllers/configuration/ClientReceptionLoggingConstants.py
+++
@@ -1,6 +0,0 @@
-from .LoggingConstants import LoggingConstants
-
-class ClientReceptionLoggingConstants(LoggingConstants):
-    def __init__(self):
-        super().__init__()
-        self.LOGNAME = "ClientHandler"
