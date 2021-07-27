8112ebce3a014cd286bff316a6f3fc2c2f9507ef
naman108 <natha.paquette@gmail.com>
2021-07-27 13:18:18 -0300

    Update setup.py
    temporary version change for testing

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 setup(
     name='FreeTAKServer',
     packages=find_packages(include = ['FreeTAKServer', 'FreeTAKServer.*']),
-    version='1.9.1',
+    version='0.1.9.1.5',
     license='Eclipse License',
     description='An open source server for the TAK family of applications.',
     long_description=long_description,
