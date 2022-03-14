cfcb25b6384fe7496b5ab03a375f6c0b6fac6c92
naman <natha.paquette@gmail.com>
2022-03-14 13:22:44 -0300

    fixed SSL service

--- FreeTAKServer/controllers/configuration/ReceiveConnectionsConstants.py
+++ FreeTAKServer/controllers/configuration/ReceiveConnectionsConstants.py
@@ -1,4 +1,5 @@
 class ReceiveConnectionsConstants:
     def __init__(self):
         self.RECEIVECONNECTIONDATATIMEOUT = 4
+        self.CONNECTION_DATA_BUFFER = 1024
         self.TESTDATA = 'TEST'
