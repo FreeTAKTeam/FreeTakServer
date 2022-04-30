1d4f0f023ab2aee6bbbe785dc87e261f8c653e92
janseptaugust <5193877+janseptaugust@users.noreply.github.com>
2022-04-30 19:10:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Eclipse Public License v2.0',
         'Programming Language :: Python :: 3.8',
     ],
 )
