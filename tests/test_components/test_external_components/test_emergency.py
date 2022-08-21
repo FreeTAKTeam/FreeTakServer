7fa0b01646b3729bf122f5b2dbebf502a6262f76
naman108 <natha.paquette@gmail.com>
2022-08-21 11:28:17 -0300

    updated test to better reflect real use case

--- tests/test_components/test_external_components/test_emergency.py
+++ tests/test_components/test_external_components/test_emergency.py
@@ -3,6 +3,7 @@ from digitalpy.config.impl.inifile_configuration import InifileConfiguration
 from digitalpy.core.object_factory import ObjectFactory
 from unittest.mock import MagicMock
 from FreeTAKServer.components.extended.emergency.emergency_constants import TYPE_MAPPINGS
+from FreeTAKServer.controllers.XMLCoTController import XMLCoTController
 from lxml import etree
 
 def setup_module(module):
@@ -33,31 +34,24 @@ def setup_module(module):
     actionmapper.process_action(request, response)
 
 def test_emergency_alert():
+    
+    
     test_data = '<event version="2.0" uid="S-1-5-21-2720623347-3037847324-4167270909-1002-9-1-1" type="b-a-o-tbl" time="2022-08-13T01:30:40.83Z" start="2022-08-13T01:30:40.83Z" stale="2022-08-13T01:40:40.83Z" how="h-e"><point lat="27.0196007365431" lon="-41.1839609383656" hae="9999999" ce="9999999" le="9999999" /><detail><link type="a-f-G-U-C-I" uid="S-1-5-21-2720623347-3037847324-4167270909-1002" relation="p-p" /><contact callsign="DATA-Alert" /><emergency type="Alert">DATA</emergency></detail></event>'
     mock_message = MagicMock()
     mock_message.xmlString = test_data
+    mock_message.clientInformation = "test"
     
     mock_client = MagicMock()
     
-    request = ObjectFactory.get_new_instance('request')
+    XMLCoTController(MagicMock()).determineCoTGeneral(mock_message, {'test': [mock_client, MagicMock()]})
     
-    request.set_context('COT')
-    request.set_action('b-a-o-tbl')
-    request.set_value('message', mock_message)
-    request.set_value('sender', '')
-    request.set_value('clients', {'test': [mock_client, MagicMock()]})
-    request.set_value('model_object_parser', "ParseModelObjectToXML")
-    
-    response = ObjectFactory.get_new_instance('response')
-    actionmapper = ObjectFactory.get_instance('actionMapper')
-
-    actionmapper.process_action(request, response)
     assert len(mock_client.send.call_args[0][0].decode()) == len(etree.tostring(etree.fromstring(test_data)))
     
 def test_emergency_broadcast():
     test_data = '<event version="2.0" uid="S-1-5-21-2720623347-3037847324-4167270909-1002-9-1-1" type="b-a-o-tbl" time="2022-08-13T01:30:40.83Z" start="2022-08-13T01:30:40.83Z" stale="2022-08-13T01:40:40.83Z" how="h-e"><point lat="27.0196007365431" lon="-41.1839609383656" hae="9999999" ce="9999999" le="9999999" /><detail><link type="a-f-G-U-C-I" uid="S-1-5-21-2720623347-3037847324-4167270909-1002" relation="p-p" /><contact callsign="DATA-Alert" /><emergency type="Alert">DATA</emergency></detail></event>'
     mock_message = MagicMock()
     mock_message.xmlString = test_data
+    mock_message.clientInformation = "test"
     
     mock_client = MagicMock()
     
