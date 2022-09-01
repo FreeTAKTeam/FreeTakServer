64ed78441eeafca46079b4f5488598f50475f9f0
naman108 <natha.paquette@gmail.com>
2022-08-31 22:59:19 -0300

    updated version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, "README.md")) as f:
 setup(
     name="FreeTAKServer",
     packages=find_packages(include=["FreeTAKServer", "FreeTAKServer.*"]),
-    version="1.9.9.8",
+    version="1.9.9.10",
     license="EPL-2.0",
     description="An open source server for the TAK family of applications.",
     long_description=long_description,
