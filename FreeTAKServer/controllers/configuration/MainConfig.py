bd95ebe1d5b75a4fd289c77dcf90bc5248857b34
naman108 <natha.paquette@gmail.com>
2022-09-01 12:26:30 -0300

    updated version

--- FreeTAKServer/controllers/configuration/MainConfig.py
+++ FreeTAKServer/controllers/configuration/MainConfig.py
@@ -12,7 +12,7 @@ class MainConfig:
     """
 
     # the version information of the server (recommended to leave as default)
-    version = "FreeTAKServer-1.9.9.10 Public"
+    version = "FreeTAKServer-1.9.9.11 Public"
     #
     yaml_path = str(os.environ.get("FTS_CONFIG_PATH", "/opt/FTSConfig.yaml"))
 
