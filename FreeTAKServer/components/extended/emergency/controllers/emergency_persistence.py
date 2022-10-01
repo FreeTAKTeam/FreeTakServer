7a55ea231927f02f1ec0afa8d5080b43d5559e3f
naman108 <natha.paquette@gmail.com>
2022-09-30 23:13:04 -0300

    added persistent emergencies to support async routing system

--- FreeTAKServer/components/extended/emergency/controllers/emergency_persistence.py
+++ FreeTAKServer/components/extended/emergency/controllers/emergency_persistence.py
@@ -1,11 +1,34 @@
 """this file contains the emergency component's persistency layer"""
+from ..configuration.emergency_constants import PERSISTENCE_PATH
 from digitalpy.routing.controller import Controller
+import json
+import os
+import pickle
 
 
 class EmergencyPersistence(Controller):
     """this class is responsible for managing saved emergencies"""
 
-    emergencies = {}
+    def __init__(self, request, response, action_mapper, configuration):
+        super().__init__(
+            request=request,
+            response=response,
+            action_mapper=action_mapper,
+            configuration=configuration,
+        )
+        self._persistence = {
+            "emergencies": {},
+        }
+
+        # create the mapping persistence if it doesn't exist already
+        if not os.path.exists(PERSISTENCE_PATH):
+            with open(PERSISTENCE_PATH, mode="w+", encoding="utf-8") as f:
+                json.dump(self._persistence, f)
+
+        # load the mapping persistence into memory
+        with open(PERSISTENCE_PATH, mode="r+", encoding="utf-8") as f:
+            self._persistence = json.load(f)
+            self.emergencies = self._persistence["emergencies"]
 
     def execute(self, method=None):
         getattr(self, method)(**self.request.get_values())
@@ -19,10 +42,11 @@ class EmergencyPersistence(Controller):
         """
         try:
             emergency_uid = model_object.uid
-            self.emergencies[emergency_uid] = model_object
+            self.emergencies[emergency_uid] = str(pickle.dumps(model_object))
             self.request.get_value("logger").debug(
                 f"added emergency: {emergency_uid} to emergencies: {self.emergencies}"
             )
+            self._update_persistence()
         except Exception as error:
             self.request.get_value("logger").error(
                 f"error adding emergency to emergencies {error}"
@@ -35,8 +59,25 @@ class EmergencyPersistence(Controller):
             emergency (Event): the emergency delete model object
         """
         del self.emergencies[model_object.uid]
+        self._update_persistence()
 
     def get_all_emergencies(self, **kwargs) -> None:
         """this method is gets all the saved emergency objects and returns them
         as a list of emergency objects"""
-        self.response.set_value("emergencies", list(self.emergencies.values()))
+        self._sync_persistence()
+        emergencies = [
+            pickle.dumps(bytes(value)) for value in self.emergencies.values()
+        ]
+        self.response.set_value("emergencies", list(emergencies))
+
+    def _sync_persistence(self):
+        with open(PERSISTENCE_PATH, mode="r", encoding="utf-8") as f:
+            self._persistence = json.load(f)
+
+    def _update_persistence(self):
+        # TODO: this form of persistence runs the risk of causing deletions
+        # if it is being written to at the same time, to avoid this we
+        # should be using a real database or at least a recognized
+        # (preferably ACID) persistence mechanism
+        with open(PERSISTENCE_PATH, mode="w", encoding="utf-8") as f:
+            json.dump(self._persistence, f)
