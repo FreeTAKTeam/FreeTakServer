dc8f20132da641339519fe1bde59e4cc4f37121a
naman108 <natha.paquette@gmail.com>
2022-09-30 23:11:25 -0300

    updated memory mapping to read mappings from a configuration originally

--- FreeTAKServer/components/core/type/controllers/memory_mapping.py
+++ FreeTAKServer/components/core/type/controllers/memory_mapping.py
@@ -1,21 +1,37 @@
 from .mapping_interface import MappingInterface
+from ..configuration.type_constants import PERSISTENCE_PATH
 from digitalpy.routing.controller import Controller
+import json
+import os
 
 
 class MemoryMapping(MappingInterface, Controller):
-
-    machine_to_human_mapping = {}
-    human_to_machine_mapping = {}
-
-    def __init__(self, request, response, action_mapper, configuration):
+    def __init__(self, request, response, sync_action_mapper, configuration):
         super().__init__(
             request=request,
             response=response,
-            action_mapper=action_mapper,
+            action_mapper=sync_action_mapper,
             configuration=configuration,
         )
-        self.machine_to_human_mapping = MemoryMapping.machine_to_human_mapping
-        self.human_to_machine_mapping = MemoryMapping.human_to_machine_mapping
+        self._persistence = {
+            "machine_to_human_mapping": {},
+            "human_to_machine_mapping": {},
+        }
+
+        # create the mapping persistence if it doesn't exist already
+        if not os.path.exists(PERSISTENCE_PATH):
+            with open(PERSISTENCE_PATH, mode="w+", encoding="utf-8") as f:
+                json.dump(self._persistence, f)
+
+        # load the mapping persistence into memory
+        with open(PERSISTENCE_PATH, mode="r+", encoding="utf-8") as f:
+            self._persistence = json.load(f)
+            self.machine_to_human_mapping = self._persistence[
+                "machine_to_human_mapping"
+            ]
+            self.human_to_machine_mapping = self._persistence[
+                "human_to_machine_mapping"
+            ]
 
     def execute(self, method=None):
         getattr(self, method)(**self.request.get_values())
@@ -36,8 +52,14 @@ class MemoryMapping(MappingInterface, Controller):
         self, machine_to_human_mapping: dict, **kwargs
     ):
         self.machine_to_human_mapping.update(machine_to_human_mapping)
+        self._update_persistence()
 
     def register_human_to_machine_mapping(
         self, human_to_machine_mapping: dict, **kwargs
     ):
         self.human_to_machine_mapping.update(human_to_machine_mapping)
+        self._update_persistence()
+
+    def _update_persistence(self):
+        with open(PERSISTENCE_PATH, mode="w", encoding="utf-8") as f:
+            json.dump(self._persistence, f)
