dfb1c2b30c617dd498c9f52ece8105942ff67fb5
naman108 <natha.paquette@gmail.com>
2022-10-11 16:38:53 -0300

    updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(
         include=["FreeTAKServer", "FreeTAKServer.*", "*.json", "*.ini", "*.conf"]
     ),
-    version="1.9.9.18",
+    version="1.9.9.20",
     license="EPL-2.0",
     description="An open source server for the TAK family of applications.",
     long_description=long_description,
