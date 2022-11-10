67560d35fd5ceddce390a633ee1a8678f3c2ddd9
Gerard Hickey <hickey@kinetic-compute.com>
2022-11-09 22:06:36 -0800

    Mass update of code accessing MainConfig
    Signed-off-by: Gerard Hickey <hickey@kinetic-compute.com>

--- FreeTAKServer/controllers/configuration/DatabaseConfiguration.py
+++ FreeTAKServer/controllers/configuration/DatabaseConfiguration.py
@@ -1,9 +1,12 @@
 from FreeTAKServer.controllers.configuration.MainConfig import MainConfig
 
+# Make a connection to the MainConfig object for all routines below
+config = MainConfig.instance()
+
 class DatabaseConfiguration:
-    DataBasePath = MainConfig.DBFilePath
-    if MainConfig.DataBaseType == "SQLite":
+    DataBasePath = config.DBFilePath
+    if config.DataBaseType == "SQLite":
         DataBaseType = str('sqlite:///')
-    elif MainConfig.DataBaseType == "MySQL":
+    elif config.DataBaseType == "MySQL":
         DataBaseType = str('mysql://')
     DataBaseConnectionString = str(DataBaseType+DataBasePath)
