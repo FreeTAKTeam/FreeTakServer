509bbb26ea2257fb6d97c0c8124fc3e4d1b3ee9a
Ariel De La Cruz <ariel.delacruz@techngs.com>
2022-11-15 14:27:11 -0500

    Update AllowedCLIIPs in MainConfig

--- FreeTAKServer/controllers/configuration/MainConfig.py
+++ FreeTAKServer/controllers/configuration/MainConfig.py
@@ -181,7 +181,7 @@ class MainConfig:
     APIPort = int(os.environ.get("FTS_API_PORT", APIPort))
     APIIP = os.environ.get("FTS_API_ADDRESS", APIIP)
     FederationPort = int(os.environ.get("FTS_FED_PORT", FederationPort))
-    AllowedCLIIPs = re.split(r'[,:]', os.environ.get("FTS_CLI_WHITELIST")) or AllowedCLIIPs
+    AllowedCLIIPs = re.split(r'[,:]', os.environ.get("FTS_CLI_WHITELIST", "")) or AllowedCLIIPs
     CLIIP = os.environ.get("FTS_CLI_IP", CLIIP)
     DBFilePath = os.environ.get("FTS_DB_PATH", DBFilePath)
     SaveCoTToDB = bool(os.environ.get("FTS_COT_TO_DB", SaveCoTToDB))
