b1d99c41a68c344dbb91f26da37567514b1950b8
naman108 <natha.paquette@gmail.com>
2022-11-21 15:49:03 -0400

    deleted broken example

--- FreeTAKServer/components/core/abstract_component/cot_node.py
+++ FreeTAKServer/components/core/abstract_component/cot_node.py
@@ -14,18 +14,6 @@ class CoTNode(Node, FTSProtocolObject):
         methods = inspect.getmembers(cls)
         return [m[0] for m in methods if getattr(m[1], "is_cot", False)]
 
-    def turbo(self):
-        type_mappings = db.query("SELECT * FROM type_table")
-        self.type_table = type_mappings
-
-    def get_internal_type(self, type):
-        if cache == True:
-            if self.type_table.get(type, None) != None:
-                return self.type_table[type]
-        human_readable_type = db.query(f"SELECT * FROM type_table WHERE type=='{type}'")
-        self.type_table[type] = human_readable_type
-        return human_readable_type
-
     def add_child(self, child):
         if self.validate_child_addition(child):
             self._children[child.get_oid().str_val] = child
