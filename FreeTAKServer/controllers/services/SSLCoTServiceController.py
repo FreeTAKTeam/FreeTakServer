c4361a94ce19b4585cef2e68e6387e084ff739f7
naman108 <natha.paquette@gmail.com>
2022-11-23 20:00:50 -0400

    fixed bugs in Orchestrator, SSLCoTServiceController and TCPCoTServiceController causing startup and connections to fail

--- FreeTAKServer/controllers/services/SSLCoTServiceController.py
+++ FreeTAKServer/controllers/services/SSLCoTServiceController.py
@@ -6,48 +6,75 @@ from FreeTAKServer.controllers.SSLSocketController import SSLSocketController
 from multiprocessing.pool import ThreadPool
 from FreeTAKServer.controllers.configuration.LoggingConstants import LoggingConstants
 from FreeTAKServer.controllers.CreateLoggerController import CreateLoggerController
-from FreeTAKServer.controllers.DatabaseControllers.DatabaseController import DatabaseController
+from FreeTAKServer.controllers.DatabaseControllers.DatabaseController import (
+    DatabaseController,
+)
 from FreeTAKServer.model.Enumerations.connectionTypes import ConnectionTypes
 from FreeTAKServer.model.SSLConnection import SSLConnection
 from FreeTAKServer.model.ClientInformation import ClientInformation
 from FreeTAKServer.model.SpecificCoT.Presence import Presence
 
 loggingConstants = LoggingConstants(log_name="FTS-SSL_CoT_Service")
-logger = CreateLoggerController("FTS-SSL_CoT_Service", logging_constants=loggingConstants).getLogger()
+logger = CreateLoggerController(
+    "FTS-SSL_CoT_Service", logging_constants=loggingConstants
+).getLogger()
+
 
 class SSLCoTServiceController(Orchestrator):
     @property
     def connection_type(self):
         return ConnectionTypes.SSL
 
-    def start(self, IP, CoTPort, Event, clientDataPipe, ReceiveConnectionKillSwitch, RestAPIPipe, clientDataRecvPipe):
+    def start(
+        self,
+        IP,
+        CoTPort,
+        Event,
+        clientDataPipe,
+        ReceiveConnectionKillSwitch,
+        RestAPIPipe,
+        clientDataRecvPipe,
+    ):
         try:
             self.logger = logger
             self.dbController = DatabaseController()
-            print('ssl cot service starting')
-            os.chdir('../../')
+            print("ssl cot service starting")
+            os.chdir("../../")
             # create socket controller
             self.SSLSocketController = SSLSocketController()
             self.SSLSocketController.changeIP(IP)
             self.SSLSocketController.changePort(CoTPort)
             sock = self.SSLSocketController.createSocket()
-            #threadpool is used as it allows the transfer of SSL socket unlike processes
+            # threadpool is used as it allows the transfer of SSL socket unlike processes
             pool = ThreadPool(processes=2)
             self.clientDataRecvPipe = clientDataRecvPipe
             self.pool = pool
-            clientData = pool.apply_async(ClientReceptionHandler().startup, (self.clientInformationQueue,))
+            clientData = pool.apply_async(
+                ClientReceptionHandler().startup, (self.clientInformationQueue,)
+            )
             receiveConnection = pool.apply_async(ReceiveConnections().listen, (sock,))
             # instantiate domain model and save process as object
-            self.mainRunFunction(clientData, receiveConnection, sock, pool, Event, clientDataPipe,
-                                 ReceiveConnectionKillSwitch, RestAPIPipe, True)
+            self.mainRunFunction(
+                clientData,
+                receiveConnection,
+                sock,
+                pool,
+                Event,
+                clientDataPipe,
+                ReceiveConnectionKillSwitch,
+                RestAPIPipe,
+                True,
+            )
         except Exception as e:
             print(e)
-            logger.error("there has been an exception thrown in"
-                         " the starting of the ssl service " + str(e))
+            logger.error(
+                "there has been an exception thrown in"
+                " the starting of the ssl service " + str(e)
+            )
             return e
 
     def add_service_user(self, client_information: ClientInformation):
-        """ this method generates the presence and connection objects from the
+        """this method generates the presence and connection objects from the
         client_information parameter and sends it to the clientDataPipe for processing
 
         :param client_information: this is the information of the client to be added
@@ -71,10 +98,17 @@ class SSLCoTServiceController(Orchestrator):
 
                 # Updating clientDataPipe
                 # TODO add blocking...
-                self.clientDataPipe.put(['add', presence_object, self.openSockets, self.connection_object])
-                self.logger.debug("client addition has been sent through queue " + str(client_information))
+                self.clientDataPipe.put(
+                    ["add", presence_object, self.openSockets, connection_object]
+                )
+                self.logger.debug(
+                    "client addition has been sent through queue "
+                    + str(client_information)
+                )
             else:
                 self.logger.critical("client data pipe is Full !")
         except Exception as e:
-            self.logger.error("exception has been thrown adding client data from queue " + str(e))
+            self.logger.error(
+                "exception has been thrown adding client data from queue " + str(e)
+            )
             raise e
