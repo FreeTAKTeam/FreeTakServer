17d5aa771810b8965fab31b6f7362fe3e30faa9b
naman108 <natha.paquette@gmail.com>
2022-11-25 13:05:38 -0400

    fixed bugs with geo_manager_controller

--- FreeTAKServer/controllers/geo_manager_controller.py
+++ FreeTAKServer/controllers/geo_manager_controller.py
@@ -1,6 +1,7 @@
 from FreeTAKServer.controllers.configuration.MainConfig import MainConfig
 
 import pickle
+import copy
 
 config = MainConfig.instance()
 
@@ -9,5 +10,16 @@ class GeoManagerController:
     @staticmethod
     def update_users(users: dict) -> None:
         """update the users object"""
-        with open(config.UserPersistencePath, "w") as f:
-            pickle.dump(users, f)
+        serialiable_users = GeoManagerController._ensure_serializable(users)
+        with open(config.UserPersistencePath, "wb") as f:
+            pickle.dump(serialiable_users, f)
+
+    @staticmethod
+    def _ensure_serializable(users: dict):
+        serializable = {}
+        for user_id, user in users.items():
+            user_c = copy.deepcopy(user[1])
+            if hasattr(user_c, "socket"):
+                user_c.socket = None
+            serializable[user_id] = user_c
+        return serializable
