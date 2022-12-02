54062e0637ab707b7954fc7334fc2ac068170298
Ariel De La Cruz <ariel.delacruz@techngs.com>
2022-12-02 08:38:01 -0500

    Move BasicModelInstantiate into domain

--- FreeTAKServer/controllers/ClientInformationController.py
+++ FreeTAKServer/controllers/ClientInformationController.py
@@ -6,7 +6,7 @@
 # Created on:      19-May-2020 6:56:00 PM
 #
 #######################################################
-from FreeTAKServer.controllers.BasicModelInstantiate import BasicModelInstantiate
+from FreeTAKServer.controllers.domian.BasicModelInstantiate import BasicModelInstantiate
 import uuid
 from FreeTAKServer.controllers.configuration.LoggingConstants import LoggingConstants
 from FreeTAKServer.controllers.CreateLoggerController import CreateLoggerController
