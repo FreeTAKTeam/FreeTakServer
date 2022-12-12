ff4e804a2ef65172efa51c861640f9de82d89e85
naman108 <natha.paquette@gmail.com>
2022-12-12 16:34:37 -0400

    removed duplicate class

--- FreeTAKServer/controllers/persistence/DataPackageTableController.py
+++
@@ -1,8 +0,0 @@
-from FreeTAKServer.core.persistence.table_controllers import TableController
-from FreeTAKServer.model.SQLAlchemy.DataPackage import DataPackage
-
-
-class DataPackageTableController(TableController):
-		
-	def __init__(self):
-		self.table = DataPackage
