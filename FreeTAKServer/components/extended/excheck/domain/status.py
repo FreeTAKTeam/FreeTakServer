6ef8a71c4d947f309fa8ea781d657fb04f0c6def
Ariel De La Cruz <ariel.delacruz@techngs.com>
2022-12-19 15:05:19 -0500

    Finish excheck domain

---
+++ FreeTAKServer/components/extended/excheck/domain/status.py
@@ -0,0 +1,6 @@
+from FreeTAKServer.components.core.abstract_component.cot_node import CoTNode
+
+
+class status(CoTNode):
+    def __init__(self, configuration, model):
+        super().__init__(self.__class__.__name__, configuration, model)
