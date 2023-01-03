f5b7f9241dc8533b524c2ea7748b6988bf391d10
Giu Platania <giu.platania@doorbusinessConsulting.com>
2023-01-03 13:34:03 -0400

    updated setup for 3.11 compatibility

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "itsdangerous==2.0.1",
         "testresources==2.0.1",
         "Jinja2==2.11.2",
-        "lxml==4.9.1",
+        "lxml",
         "MarkupSafe==2.0.1",
         "monotonic==1.6",
         "pathlib2==2.3.7.post1",
