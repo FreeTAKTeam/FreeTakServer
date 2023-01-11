bbe7c3e27a28d5eb7322235d18a6a6e4e20f6bcb
naman108 <natha.paquette@gmail.com>
2023-01-11 12:03:39 -0400

    updated version

--- FreeTAKServer/core/configuration/MainConfig.py
+++ FreeTAKServer/core/configuration/MainConfig.py
@@ -8,7 +8,7 @@ from uuid import uuid4
 
 # the version information of the server (recommended to leave as default)
 
-FTS_VERSION = "FreeTAKServer-1.9.10.8 Public"
+FTS_VERSION = "FreeTAKServer-2.0.1 Alpha"
 API_VERSION = "1.9.5"
 # TODO Need to find a better way to determine python version at runtime
 PYTHON_VERSION = "python3.8"
