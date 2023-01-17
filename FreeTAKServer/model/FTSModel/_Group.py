b78287a7b9d6e78d57a107ad56c8d86c66699295
naman108 <natha.paquette@gmail.com>
2023-01-17 10:57:45 -0400

    fixed model indentation issues

--- FreeTAKServer/model/FTSModel/_Group.py
+++ FreeTAKServer/model/FTSModel/_Group.py
@@ -4,7 +4,28 @@ class _Group(FTSProtocolObject):
     def __init__(self):
         self.name = None
         self.role = None
+        
+    @staticmethod
+    def connection(NAME = vars.connection().NAME, ROLE = vars.connection().ROLE):
+        _group = _Group()
+        _group.setname(NAME)
+        _group.setrole(ROLE)
+        return _group
 
+    @staticmethod
+    def UserUpdate(NAME=vars.UserUpdate().name, ROLE=vars.UserUpdate().role):
+        _group = _Group()
+        _group.setname(NAME)
+        _group.setrole(ROLE)
+        return _group
+
+    @staticmethod
+    def Presence(NAME=vars.Presence().name, ROLE=vars.Presence().role):
+        _group = _Group()
+        _group.setname(NAME)
+        _group.setrole(ROLE)
+        return _group
+    
     def getrole(self):
         return self.role
 
