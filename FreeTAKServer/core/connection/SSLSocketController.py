7a90d0277485257025f9610a51d584a26d257894
naman108 <natha.paquette@gmail.com>
2023-01-17 18:06:31 -0400

    disallowed all ssl/tls versions below TLS1_2

--- FreeTAKServer/core/connection/SSLSocketController.py
+++ FreeTAKServer/core/connection/SSLSocketController.py
@@ -38,6 +38,8 @@ class SSLSocketController(MainSocketController):
         context.load_verify_locations(cafile=self.MainSocket.CA)
         context.options |= ssl.OP_NO_SSLv3
         context.options |= ssl.OP_NO_SSLv2
+        context.options |= ssl.OP_NO_TLSv1
+        context.options |= ssl.OP_NO_TLSv1_1
         context.verify_mode = ssl.CERT_REQUIRED
         context.verify_flags = ssl.VERIFY_CRL_CHECK_LEAF
         context.load_cert_chain(certfile=self.MainSocket.pemDir, keyfile=self.MainSocket.keyDir,
@@ -51,6 +53,8 @@ class SSLSocketController(MainSocketController):
         context.load_cert_chain(certfile=self.MainSocket.pemDir, keyfile=self.MainSocket.keyDir,password=self.MainSocket.password)
         context.options |= ssl.OP_NO_SSLv2
         context.options |= ssl.OP_NO_SSLv3
+        context.options |= ssl.OP_NO_TLSv1
+        context.options |= ssl.OP_NO_TLSv1_1
         context.check_hostname = False
         self.MainSocket.sock = socket.socket(self.MainSocket.socketAF, self.MainSocket.socketSTREAM)
         self.MainSocket.sock = context.wrap_socket(self.MainSocket.sock)
