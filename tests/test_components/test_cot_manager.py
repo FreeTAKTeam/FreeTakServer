5330348d2c415cec1d878e6abadce82fdfd74b99
Ariel De La Cruz <ariel.delacruz@techngs.com>
2023-02-03 07:58:14 -0500

    WIP

--- tests/test_components/test_cot_manager.py
+++ tests/test_components/test_cot_manager.py
@@ -1,12 +1,13 @@
-from unittest.mock import Mock, patch, MagicMock
+import json
 import uuid
-import pickle
-from typing import Dict
+from unittest.mock import MagicMock, Mock, patch
+
+from digitalpy.core.main.controller import Controller
 from digitalpy.core.zmanager.impl.default_request import DefaultRequest
 from digitalpy.core.zmanager.impl.default_response import DefaultResponse
-from digitalpy.core.main.controller import Controller
-from digitalpy.core.main.object_factory import ObjectFactory
+
 from FreeTAKServer.core.cot_management.cot_management_facade import CotManagement
+
 
 def setup():
     pass
@@ -24,11 +25,14 @@ def get_mock_node(oid = get_mock_oid()):
     mock_node.get_oid.return_value = oid
     return mock_node
 
-def get_mock_connection(node = get_mock_node(), service_id = "test-service", protocol = "test-protocol"):
-    mock_connection = node
-    mock_connection.service_id = "test_service"
-    mock_connection.protocol = "test_protocol"
-    return mock_connection
+def get_mock_node_from_obj(obj: dict, oid = get_mock_oid()):
+    mock_node = Mock()
+    mock_node.get_oid.return_value = oid
+
+    for key in obj:
+        setattr(mock_node, key, obj[key])
+
+    return mock_node
 
 def instantiate_request_response(action):
     request = DefaultRequest()
@@ -44,31 +48,96 @@ def assert_response_val(value_name, value_type, value_content, response):
     assert isinstance(response.get_value(value_name), value_type)
     assert response.get_value(value_name) == value_content
 
-def assert_saved(persistency_save_method: callable, value):
-    assert value in persistency_save_method.mock_calls[0].args
+def assert_schema_to_response_val(response_schema_values, response):
+    for key, value in response_schema_values.items():
+        # assert that the value exists in component response
+        assert response.get_value(key) is not None
+        # assert that they are the correct type
+        assert isinstance(response.get_value(key), type(value))
+
+        # TODO if they are mock obj then only compare ids
+        # assert that they are the same
+        assert response.get_value(key) == value
+
+def parse_schema(schema: str):
+    return json.loads(schema)
+
+def get_request_response_from_test_obj(test_obj):
+    request = DefaultRequest()
+    response = DefaultResponse()
+
+    # if value is a node object, create mock node
+    for key in test_obj['request']['values']:
+        value = test_obj['request']['values'][key]
+        if isinstance(value, list):
+            test_obj['request']['values'][key] = r_convert_values_to_node(value)
+        elif value['is_node']:
+            test_obj['request']['values'][key] = get_mock_node_from_obj(value, value['oid'])
+
+    # set request action and values
+    request.set_action(test_obj['request']['action'])
+    request.set_values(test_obj['request']['values'])
+
+    # set response action
+    response.set_action(test_obj['response']['action'])
+
+    return request, response
+
+def r_convert_values_to_node(value):
+    base = value[0]
+
+    if isinstance(base, list):
+        r_convert_values_to_node(value[0])
+        
+    if isinstance(base, dict) and base['is_node']:
+        return [get_mock_node_from_obj(base, base['oid'])]
+
+    # assume value is list of non node items aka strings
+    return value
+
+test_connection_schema = """
+{
+    "request": {
+        "values": {
+            "connection": {
+                "oid": "3b1a979c-a31b-11ed-a8fc-0242ac120002",
+                "service_id": "test_service",
+                "protocol": "test_protocol",
+                "is_node": true 
+            }
+        },
+        "action": "connection"
+    },
+    "response": {
+        "action": "publish",
+        "values": {
+            "message": [
+
+            ]
+        }
+    },
+    "node_id": "3b1a979c-a31b-11ed-a8fc-0242ac120002"
+}
+"""
 
 # patch the persistency output
 @patch('pickle.load')
 def test_connection(mock_load):
     """test the connection action in the cot manager
     """
+    test_obj = parse_schema(test_connection_schema)
+
     # instnatiate request and response objects
-    request, response = instantiate_request_response("connection")
+    request, response = get_request_response_from_test_obj(test_obj)
 
     # mock the execute sub action method in the controller class
     mock_controller_execute_sub_action(response)
 
     # get a mock node object
-    mock_node = get_mock_node()
+    mock_node = get_mock_node(oid=test_obj['node_id'])
 
     # define the output dictionary of the mocked persistency output
     mock_load.return_value = {str(mock_node.get_oid()): mock_node}
-
-    # instantiate a mock connection object
-    mock_connection = get_mock_connection()
-
-    # set the content of the connection value to be a mock connection
-    request.set_value("connection", mock_connection)
 
     # instantiate the facade
     facade = CotManagement(None, request, response, None)
@@ -80,21 +149,37 @@ def test_connection(mock_load):
     facade.connection(**request.get_values())
 
     # assert that the next action is publish
-    assert response.get_action() == "publish"
+    assert response.get_action() == test_obj['response']['action']
+
     # assert the message value is correct
     assert_response_val('message', list, [mock_node], response)
-    
+
+test_get_repeated_messages_schema = """
+{
+    "request": {
+        "values": {},
+        "action": "GetRepeatedMessages"
+    },
+    "response":{
+        "action": "GetRepeatedMessages"
+    },
+    "node_id": "672a39ee-a31d-11ed-a8fc-0242ac120002"
+}
+"""
+
 @patch('pickle.load')
 def test_get_repeated_messages(mock_load):
 
+    test_obj = parse_schema(test_get_repeated_messages_schema)
+
     # instnatiate request and response objects
-    request, response = instantiate_request_response("GetRepeatedMessages")
+    request, response = get_request_response_from_test_obj(test_obj)
 
     # mock the execute sub action method in the controller class
     mock_controller_execute_sub_action(response)
 
     # get a mock node object
-    mock_node = get_mock_node()
+    mock_node = get_mock_node(oid=test_obj['node_id'])
 
     # define the output dictionary of the mocked persistency output
     mock_load.return_value = {str(mock_node.get_oid()): mock_node}
@@ -109,28 +194,47 @@ def test_get_repeated_messages(mock_load):
     facade.get_repeated_messages(**request.get_values())
 
     # assert that the next action is GetRepeatedMessages thus resulting in no further actions being called
-    assert response.get_action() == "GetRepeatedMessages"
+    assert response.get_action() == test_obj['response']['action']
+
     # assert the message value is correct
     assert_response_val('message', list, [mock_node], response)
+
+test_create_repeated_message_schema = """
+{
+    "request": {
+        "values": {
+            "message": [
+                {
+                    "oid": "59886b34-a31e-11ed-a8fc-0242ac120002",
+                    "is_node": true
+                }
+            ]
+        },
+        "action": "CreateRepeatedMessage"
+    },
+    "response": {
+        "action": "CreateRepeatedMessage",
+        "values": {
+            "success": true
+        }
+    }
+}
+"""
 
 @patch('pickle.dump')
 @patch('pickle.load')
 def test_create_repeated_message(mock_load, mock_dump):
 
+    test_obj = parse_schema(test_create_repeated_message_schema)
+
     # instnatiate request and response objects
-    request, response = instantiate_request_response("CreateRepeatedMessage")
+    request, response = get_request_response_from_test_obj(test_obj)
 
     # mock the execute sub action method in the controller class
     mock_controller_execute_sub_action(response)
 
-    # get a mock node object
-    mock_node = get_mock_node()
-
     # define the output dictionary of the mocked persistency output
     mock_load.return_value = {}
-
-    # set the content of the message value to be a list containing one mocked node
-    request.set_value("message", [mock_node])
 
     # instantiate the facade
     facade = CotManagement(None, request, response, None)
@@ -142,29 +246,45 @@ def test_create_repeated_message(mock_load, mock_dump):
     facade.create_repeated_message(**request.get_values())
 
     # assert that the next action is CreateRepeatedMessage
-    assert response.get_action() == "CreateRepeatedMessage"
+    assert response.get_action() == test_obj['response']['action']
+
     # assert the success value is correct
-    assert_response_val('success', bool, True, response)
-    # assert that the node was saved
-    assert_saved(mock_dump, {str(mock_node.get_oid()): mock_node})
+    assert_schema_to_response_val(test_obj['response']['values'], response)
+
+test_delete_repeated_message_schema = """
+{
+    "request": {
+        "values": {
+            "ids": [
+                "329f80aa-a2f8-11ed-a8fc-0242ac120002"
+            ]
+        },
+        "action": "DeleteRepeatedMessage"
+    },
+    "response": {
+        "action": "DeleteRepeatedMessage"
+    },
+    "node_id": "329f80aa-a2f8-11ed-a8fc-0242ac120002"
+}
+"""
 
 @patch('pickle.dump')
 @patch('pickle.load')
 def test_delete_repeated_message(mock_load, mock_dump):
+    # parse object from schema
+    test_obj = parse_schema(test_delete_repeated_message_schema)
 
-    # instnatiate request and response objects
-    request, response = instantiate_request_response("DeleteRepeatedMessage")
+    # instantiate request and response objects
+    request, response = get_request_response_from_test_obj(test_obj)
     
     # mock the execute sub action method in the controller class
     mock_controller_execute_sub_action(response)
 
     # get a mock node object
-    mock_node = get_mock_node()
+    mock_node = get_mock_node(oid=test_obj['node_id'])
 
     # define the output dictionary of the mocked persistency output
     mock_load.return_value = {str(mock_node.get_oid()): mock_node}
-
-    request.set_value("ids", [str(mock_node.get_oid())])
 
     # instantiate the facade
     facade = CotManagement(None, request, response, None)
@@ -175,18 +295,38 @@ def test_delete_repeated_message(mock_load, mock_dump):
     # call the delete_repeated_messages method from the facade
     facade.delete_repeated_message(**request.get_values())
 
-    assert response.get_action() == "DeleteRepeatedMessage"
+    # assert that the next action is the same
+    assert response.get_action() == test_obj['response']['action']
+
     # assert the success value is correct
     assert_response_val('success', bool, True, response)
-    # assert that the was deleted and an empty dictionary was saved instead
-    assert_saved(mock_dump, {})
+
+test_delete_non_existent_repeated_message = """
+{
+    "request": {
+        "values": {
+            "ids": ["329f80aa-a2f8-11ed-a8fc-0242ac120002"]
+        },
+        "action": "DeleteRepeatedMessage"
+    },
+    "response": {
+        "action": "DeleteRepeatedMessage"
+    },
+    "node_id": "329f80aa-a2f8-11ed-a8fc-0242ac120002"
+}
+"""
 
 @patch('pickle.dump')
 @patch('pickle.load')
 def test_delete_non_existent_repeated_message(mock_load, mock_dump):
+    # parse object from schema
+    test_obj = parse_schema(test_delete_repeated_message_schema)
 
-    # instnatiate request and response objects
-    request, response = instantiate_request_response("DeleteRepeatedMessage")
+    # instantiate request and response objects
+    request, response = get_request_response_from_test_obj(test_obj)
+
+    # # instnatiate request and response objects
+    # request, response = instantiate_request_response("DeleteRepeatedMessage")
     
     # mock the execute sub action method in the controller class
     mock_controller_execute_sub_action(response)
@@ -208,8 +348,6 @@ def test_delete_non_existent_repeated_message(mock_load, mock_dump):
     # call the delete_repeated_messages method from the facade
     facade.delete_repeated_message(**request.get_values())
 
-    assert response.get_action() == "DeleteRepeatedMessage"
+    assert response.get_action() == test_obj['response']['action']
     # assert the success value is correct
     assert_response_val('success', bool, True, response)
-    # assert that the was deleted and an empty dictionary was saved instead
-    assert_saved(mock_dump, {})
