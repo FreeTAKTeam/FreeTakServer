3cda99d555150daab6e35035cfb6a31d4b4c1368
Shintaro Tanaka <tallow@dirt.ninja>
2023-04-01 20:23:38 +0900

    more relaxing timeout on receive connection data

--- FreeTAKServer/core/configuration/ReceiveConnectionsConstants.py
+++ FreeTAKServer/core/configuration/ReceiveConnectionsConstants.py
@@ -1,6 +1,6 @@
 class ReceiveConnectionsConstants:
     def __init__(self):
-        self.RECEIVECONNECTIONDATATIMEOUT = 4
+        self.RECEIVECONNECTIONDATATIMEOUT = 30
         self.CONNECTION_DATA_BUFFER = 1024
         self.TESTDATA = 'TEST'
         # timeout for the wrap socket operation in seconds
