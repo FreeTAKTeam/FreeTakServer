91babe4b2d33ce1c4bcc3389abc155d5d37ab7a8
Natha Paquette <natha.paquette@gmail.com>
2023-06-20 13:07:35 -0300

    updated version

--- FreeTAKServer/core/configuration/MainConfig.py
+++ FreeTAKServer/core/configuration/MainConfig.py
@@ -11,7 +11,7 @@ from uuid import uuid4
 
 # the version information of the server (recommended to leave as default)
 
-FTS_VERSION = "FreeTAKServer-2.0.70.4"
+FTS_VERSION = "FreeTAKServer-2.0.70.5"
 API_VERSION = "3"
 ROOTPATH = "/"
 MAINPATH = Path(__file__).parent.parent.parent
